78814c09aaadf7622a122970e8a35a4f89430a8b
Felipe Moreira <felipe.g.silva@capgemini.com>
2022-09-14 10:30:08 -0300

    Created project Mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,42 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+interface ButtonProps {
+  title: string;
+}
+
+const Button = (props: ButtonProps) => {
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello Word !!!</Text>
+
+      <Button title="Send 1" />
+      <Button title="Send 2" />
+      <Button title="Send 3" />
+      <Button title="Hello Word !!!" />
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
+
+export default App;
